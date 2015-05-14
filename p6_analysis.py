7016baf0cd9138869bbac15b1a2786015a740526
SamReha <sreha@ucsc.edu>
2015-05-13 20:50:16 -0700

    Removed unused variable

--- p6_analysis.py
+++ p6_analysis.py
@@ -1,12 +1,23 @@
 from p6_game import Simulator
 
-ANALYSIS = {}
+ANALYSIS = {}   # A dictionary mapping states to the previous states that lead to them (I think!)
 
 def analyze(design):
     sim = Simulator(design)
-    # TODO: fill in this function, populating the ANALYSIS dict
-    raise NotImplementedError
+    init = sim.get_initial_state()
+    frontier = []
+    frontier.append(init)
+    ANALYSIS[init] = None
+
+    while len(frontier) > 0:
+        current = frontier.pop(0)
+        for move in sim.get_moves():
+            if move not in ANALYSIS:
+                # Record the state resulting from applying our next hypothetical move to our current state
+                frontier.append(sim.get_next_state(current, move))
+                ANALYSIS[move] = current
+                print current
 
 def inspect((i,j), draw_line):
-    # TODO: use ANALYSIS and (i,j) draw some lines
-    raise NotImplementedError
+    pass
+    
