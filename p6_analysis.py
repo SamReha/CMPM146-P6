0b06f5e56fb8ac3f7e459fcdda31a8e8e603c996
SamReha <sreha@ucsc.edu>
2015-05-13 22:27:49 -0700

    Exhaustive search ACTUALLY works now. Also, checks level completability

--- p6_analysis.py
+++ p6_analysis.py
@@ -1,23 +1,41 @@
 from p6_game import Simulator
 
-ANALYSIS = {}   # A dictionary mapping states to the previous states that lead to them (I think!)
+global ANALYSIS     # A dictionary mapping states to the previous states that lead to them (I think!)
+
+def _get_neighbors(sim, state):
+    for move in sim.get_moves():
+        new_state = sim.get_next_state(state, move)
+        if new_state:
+            yield new_state
 
 def analyze(design):
     sim = Simulator(design)
+    specials = design['specials']
     init = sim.get_initial_state()
     frontier = []
     frontier.append(init)
+    ANALYSIS = {}    # Reset the ANALYSIS dictionary to scrub out old data.
     ANALYSIS[init] = None
 
     while len(frontier) > 0:
         current = frontier.pop(0)
-        for move in sim.get_moves():
-            if move not in ANALYSIS:
-                # Record the state resulting from applying our next hypothetical move to our current state
-                frontier.append(sim.get_next_state(current, move))
-                ANALYSIS[move] = current
-                print current
+        for neighbor in _get_neighbors(sim, current):
+            if neighbor not in ANALYSIS:
+                # Record the unvisited state
+                frontier.append(neighbor)
+                ANALYSIS[neighbor] = current
+                
+    # Check if goal coordinate (Special 5) is reachable
+    reach = False
+    for state in ANALYSIS:
+        position, _ = state
+        if specials.get(position, 0) == 5:
+            reach = True
+            break  
+    if reach:
+        print "Special 5 is reachable!"
+    else:
+        print "Special 5 is NON reachable!"
 
 def inspect((i,j), draw_line):
     pass
-    
